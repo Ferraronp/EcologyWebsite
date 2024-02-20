ab07fc2833f1a879e25f121a1a2860d170533d24
ferraronp <avzal-ar@mail.ru>
2024-02-20 14:30:45 +0300

    Adding response model for getting news route

--- src/routes/news/get_news.py
+++ src/routes/news/get_news.py
@@ -5,7 +5,9 @@ from fastapi_utils.tasks import repeat_every
 from src.routes.news.parsers.ecosphere_press_parser import get_news_ecosphere_press
 from src.routes.news.parsers.eco_portal_parser import eco_portalsu_parser
 
-from src.routes.news.database.models.news_model import News
+from src.models.news import News
+
+from src.routes.news.database.models.news_model import News as NewsFromDB
 from .database import db_session
 
 
@@ -13,25 +15,25 @@ db_session.global_init("src/routes/news/database/news.db")
 router = APIRouter()
 
 
-@router.get("/get_news")
+@router.get("/get_news", response_model=list[News])
 def get_news():
     news_list = list()
     db_sess = db_session.create_session()
-    for news in db_sess.query(News).all():
-        dict_ = dict()
-        dict_['id'] = news.id
-        dict_['title'] = news.title
-        dict_['content'] = news.content
-        dict_['url'] = news.url
-        dict_['date'] = news.date
-        news_list.append(dict_)
+    for news_from_db in db_sess.query(NewsFromDB).all():
+        news = News(**news_from_db.__dict__)
+        news.id = news_from_db.id
+        news.title = news_from_db.title
+        news.content = news_from_db.content
+        news.url = news_from_db.url
+        news.date = news_from_db.date
+        news_list.append(news)
     return news_list
 
 
 @router.get("/get_news_img")
 def get_news(news_id: int):
     db_sess = db_session.create_session()
-    news_img = db_sess.query(News).filter(News.id == news_id).first().img
+    news_img = db_sess.query(NewsFromDB).filter(NewsFromDB.id == news_id).first().img
     return Response(news_img)
 
 
@@ -40,12 +42,12 @@ def get_news(news_id: int):
 def update_news():
     logging.info('Updating news in database')
     news_list = list()
-    news_list.extend(get_news_ecosphere_press())
-    news_list.extend(eco_portalsu_parser())
+    # news_list.extend(get_news_ecosphere_press())
+    # news_list.extend(eco_portalsu_parser())
     for i in news_list:
         try:
             db_sess = db_session.create_session()
-            news = News()
+            news = NewsFromDB()
             news.title = i['title']
             news.date = i['date']
             news.content = i['content']
