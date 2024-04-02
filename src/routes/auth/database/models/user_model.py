cb8ca2881a70a30876feb19615c55f91c6d71297
ferraronp <avzal-ar@mail.ru>
2024-04-02 11:33:28 +0300

    Add database to authentication, delete unnecessary routes

---
+++ src/routes/auth/database/models/user_model.py
@@ -0,0 +1,12 @@
+import sqlalchemy
+from ..db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    username = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
